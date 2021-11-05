27b9a63539af22dc628987993adb219fc4c7770f
Pavol Hejny <me@pavolhejny.com>
2021-11-05 13:40:55 +0100

    Updating exports

--- src/index.ts
+++ src/index.ts
@@ -9,31 +9,34 @@ import { NotFoundError } from './errors/NotFoundError';
 import { IDestroyable } from './IDestroyable';
 import { IRegisterItemsOptions } from './IRegisterItemsOptions';
 import { ISubscription } from './ISubscription';
-import { ITick, IWaiter, IWork } from './IWork';
+import { IWork } from './IWork';
+import { ITick } from './IWork';
+import { IWaiter } from './IWork';
 import { registerItemsInArray } from './registerItemsInArray';
 import { registerItemsInSubjectOfArrays } from './registerItemsInSubjectOfArrays';
 import { registerPairsInObject } from './registerPairsInObject';
 import { Registration } from './Registration';
 import { softDestroy } from './softDestroy';
-import { ITeardownLogic, teardown } from './TeardownLogic';
+import { ITeardownLogic } from './TeardownLogic';
+import { teardown } from './TeardownLogic';
 
 export {
-    ITick,
-    IWork,
-    IWaiter,
-    teardown,
-    softDestroy,
-    Destroyable,
-    Registration,
-    IDestroyable,
-    ISubscription,
-    NotFoundError,
-    ITeardownLogic,
-    CollisionError,
-    DestroyableLoop,
-    registerItemsInArray,
-    registerPairsInObject,
-    IRegisterItemsOptions,
-    AlreadyDestroyedError,
-    registerItemsInSubjectOfArrays,
+ITick,
+IWork,
+IWaiter,
+teardown,
+softDestroy,
+Destroyable,
+Registration,
+IDestroyable,
+ISubscription,
+NotFoundError,
+ITeardownLogic,
+CollisionError,
+DestroyableLoop,
+registerItemsInArray,
+registerPairsInObject,
+IRegisterItemsOptions,
+AlreadyDestroyedError,
+registerItemsInSubjectOfArrays
 };
